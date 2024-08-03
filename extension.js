edc7f9ba02a0350acc837272485641275b507cea
Aorih <sergioferrerept@gmail.com>
2024-08-03 22:13:19 +0200

    feat: Converted to submenu

--- extension.js
+++ extension.js
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-const path = require('path');
-const fs = require('fs');
+const vscode = require("vscode");
+const path = require("path");
+const fs = require("fs");
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -11,59 +11,51 @@ const fs = require('fs');
  */
 
 function activate(context) {
-    let showTemplates = vscode.commands.registerCommand('kubernetes-file-generator.showTemplates', async (uri) => {
-        const options = ["Create Deployment", "Create Service", "Create ConfigMap"];
-        const selected = await vscode.window.showQuickPick(options, {
-            placeHolder: "Select a Kubernetes template to create"
-        });
 
-        switch (selected) {
-            case "Create Deployment":
-                vscode.commands.executeCommand('kubernetes-file-generator.createDeployment', uri);
-                break;
-            case "Create Service":
-                vscode.commands.executeCommand('kubernetes-file-generator.createService', uri);
-                break;
-            case "Create ConfigMap":
-                vscode.commands.executeCommand('kubernetes-file-generator.createConfigMap', uri);
-                break;
-        }
-    });
+  let createDeployment = vscode.commands.registerCommand(
+    "kubernetes-file-generator.createDeployment",
+    (uri) => {
+      createKubernetesTemplate(uri, "deployment.yaml", getDeploymentTemplate());
+    }
+  );
 
-    let createDeployment = vscode.commands.registerCommand('kubernetes-file-generator.createDeployment', (uri) => {
-        createKubernetesTemplate(uri, 'deployment.yaml', getDeploymentTemplate());
-    });
+  let createService = vscode.commands.registerCommand(
+    "kubernetes-file-generator.createService",
+    (uri) => {
+      createKubernetesTemplate(uri, "service.yaml", getServiceTemplate());
+    }
+  );
 
-    let createService = vscode.commands.registerCommand('kubernetes-file-generator.createService', (uri) => {
-        createKubernetesTemplate(uri, 'service.yaml', getServiceTemplate());
-    });
+  let createConfigMap = vscode.commands.registerCommand(
+    "kubernetes-file-generator.createConfigMap",
+    (uri) => {
+      createKubernetesTemplate(uri, "configmap.yaml", getConfigMapTemplate());
+    }
+  );
 
-    let createConfigMap = vscode.commands.registerCommand('kubernetes-file-generator.createConfigMap', (uri) => {
-        createKubernetesTemplate(uri, 'configmap.yaml', getConfigMapTemplate());
-    });
-
-    context.subscriptions.push(showTemplates, createDeployment, createService, createConfigMap);
+  context.subscriptions.push(createDeployment, createService, createConfigMap);
 }
 
-
 function createKubernetesTemplate(uri, filename, content) {
-    const dirPath = uri.fsPath;
-    const filePath = path.join(dirPath, filename);
+  const dirPath = uri.fsPath;
+  const filePath = path.join(dirPath, filename);
 
-    fs.writeFile(filePath, content, (err) => {
-        if (err) {
-            vscode.window.showErrorMessage(`Failed to create ${filename}: ${err.message}`);
-            return;
-        }
-        vscode.window.showInformationMessage(`${filename} created successfully!`);
-        vscode.workspace.openTextDocument(filePath).then(doc => {
-            vscode.window.showTextDocument(doc);
-        });
+  fs.writeFile(filePath, content, (err) => {
+    if (err) {
+      vscode.window.showErrorMessage(
+        `Failed to create ${filename}: ${err.message}`
+      );
+      return;
+    }
+    vscode.window.showInformationMessage(`${filename} created successfully!`);
+    vscode.workspace.openTextDocument(filePath).then((doc) => {
+      vscode.window.showTextDocument(doc);
     });
+  });
 }
 
 function getDeploymentTemplate() {
-    return `apiVersion: apps/v1
+  return `apiVersion: apps/v1
 kind: Deployment
 metadata:
   name: my-deployment
@@ -86,7 +78,7 @@ spec:
 }
 
 function getServiceTemplate() {
-    return `apiVersion: v1
+  return `apiVersion: v1
 kind: Service
 metadata:
   name: my-service
@@ -101,7 +93,7 @@ spec:
 }
 
 function getConfigMapTemplate() {
-    return `apiVersion: v1
+  return `apiVersion: v1
 kind: ConfigMap
 metadata:
   name: my-configmap
@@ -113,6 +105,6 @@ data:
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
