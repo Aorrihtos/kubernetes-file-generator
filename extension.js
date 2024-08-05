253da3c595069a7c823e1062d6a616bb8df38fef
Aorih <sergioferrerept@gmail.com>
2024-08-05 09:04:22 +0200

    feat: Name placeholder

--- extension.js
+++ extension.js
@@ -14,82 +14,98 @@ function activate(context) {
 
   let createDeployment = vscode.commands.registerCommand(
     "kubernetes-file-generator.createDeployment",
-    (uri) => {
-      createKubernetesTemplate(uri, "deployment.yaml", getTemplateContent("deployment"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-deployment.yaml`, getTemplateContent("deployment", name));
     }
   );
 
   let createService = vscode.commands.registerCommand(
     "kubernetes-file-generator.createService",
-    (uri) => {
-      createKubernetesTemplate(uri, "service.yaml", getTemplateContent("service"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-service.yaml`, getTemplateContent("service", name));
     }
   );
 
   let createConfigMap = vscode.commands.registerCommand(
     "kubernetes-file-generator.createConfigMap",
-    (uri) => {
-      createKubernetesTemplate(uri, "configmap.yaml", getTemplateContent("configmap"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-configmap.yaml`, getTemplateContent("configmap", name));
     }
   );
 
   let createCronjob = vscode.commands.registerCommand(
     "kubernetes-file-generator.cronJob",
-    (uri) => {
-      createKubernetesTemplate(uri, "cronjob.yaml", getTemplateContent("cronjob"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-cronjob.yaml`, getTemplateContent("cronjob", name));
     }
   );
 
   let createDaemonSet = vscode.commands.registerCommand(
     "kubernetes-file-generator.daemonSet",
-    (uri) => {
-      createKubernetesTemplate(uri, "daemonset.yaml", getTemplateContent("daemonset"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-daemonset.yaml`, getTemplateContent("daemonset", name));
     }
   );
 
   let createHorizontalPodAutoescaller = vscode.commands.registerCommand(
     "kubernetes-file-generator.horizontalPodAutoescaller",
-    (uri) => {
-      createKubernetesTemplate(uri, "horizontalpodautoescaller.yaml", getTemplateContent("horizontalpodautoescaller"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-horizontalpodautoescaller.yaml`, getTemplateContent("horizontalpodautoescaller", name));
     }
   );
 
   let createIngress = vscode.commands.registerCommand(
     "kubernetes-file-generator.ingress",
-    (uri) => {
-      createKubernetesTemplate(uri, "ingress.yaml", getTemplateContent("ingress"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-ingress.yaml`, getTemplateContent("ingress", name));
     }
   );
 
   let createPersistentVolume = vscode.commands.registerCommand(
     "kubernetes-file-generator.persistentVolume",
-    (uri) => {
-      createKubernetesTemplate(uri, "persistentvolume.yaml", getTemplateContent("persistentvolume"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-persistentvolume.yaml`, getTemplateContent("persistentvolume", name));
     }
   );
 
   let createPersistentVolumeClaim = vscode.commands.registerCommand(
     "kubernetes-file-generator.persistentVolumeClaim",
-    (uri) => {
-      createKubernetesTemplate(uri, "persistentvolumeclaim.yaml", getTemplateContent("persistentvolumeclaim"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-persistentvolumeclaim.yaml`, getTemplateContent("persistentvolumeclaim", name));
     }
   );
 
   let createSecret = vscode.commands.registerCommand(
     "kubernetes-file-generator.secret",
-    (uri) => {
-      createKubernetesTemplate(uri, "secret.yaml", getTemplateContent("secret"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-secret.yaml`, getTemplateContent("secret", name));
     }
   );
 
   let createStatefulSet = vscode.commands.registerCommand(
     "kubernetes-file-generator.statefulSet",
-    (uri) => {
-      createKubernetesTemplate(uri, "statefulset.yaml", getTemplateContent("statefulset"));
+    async (uri) => {
+      const name = await getComponentName();
+      createKubernetesTemplate(uri, `${name}-statefulset.yaml`, getTemplateContent("statefulset", name));
     }
   );
 
-  context.subscriptions.push(createDeployment, createService, createConfigMap);
+  context.subscriptions.push(
+    createDeployment, createService, createConfigMap, 
+    createCronjob, createDaemonSet, createHorizontalPodAutoescaller, 
+    createIngress, createPersistentVolume, createPersistentVolumeClaim, 
+    createSecret, createStatefulSet
+  );
 }
 
 function createKubernetesTemplate(uri, filename, content) {
@@ -110,8 +126,24 @@ function createKubernetesTemplate(uri, filename, content) {
   });
 }
 
-function getTemplateContent(name){
-  return fs.readFileSync(`${__dirname}/kubernetes-templates/${name}.yaml`, 'utf-8');
+function getTemplateContent(component, name){
+  return fs.readFileSync(`${__dirname}/kubernetes-templates/${component}.yaml`, 'utf-8')
+          .replace("<name>", name);
+}
+
+async function getComponentName(){
+  const userInput = await vscode.window.showInputBox({
+    placeHolder: "Enter the name of the component",
+    prompt: "Please provide the required information",
+  });
+  
+  let name="";
+
+  if(userInput != undefined){
+    name = userInput;
+  }
+
+  return name;
 }
 
 // This method is called when your extension is deactivated
